19afae69be76ce99a6e9327e04265eee504452bc
Ryan Butler <thebutlah@gmail.com>
2023-08-22 01:18:12 -0400

    Restored old kickstart.nvim keymaps

--- lua/thebutlah/plugins/lsp.lua
+++ lua/thebutlah/plugins/lsp.lua
@@ -61,11 +61,32 @@ return {
 		},
 		config = function()
 			-- This is where all the LSP shenanigans live
-
 			local lsp = require('lsp-zero').preset({})
 
 			lsp.on_attach(function(client, bufnr)
 				lsp.default_keymaps({ buffer = bufnr })
+
+				-- Helper func
+				local nmap = function(keys, func, desc)
+					if desc then
+						desc = 'LSP: ' .. desc
+					end
+					vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+				end
+
+				nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+				nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+				nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+				nmap('gD', vim.lsp.buf.type_definition, '[G]oto Type [D]efinition')
+				nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+				nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+				nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+				nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+				vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+					vim.lsp.buf.format()
+				end, { desc = 'Format current buffer with LSP' })
 			end)
 
 			require('mason-lspconfig').setup({
