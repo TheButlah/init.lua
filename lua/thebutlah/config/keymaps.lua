0fb5f793d1ae4ae42c45df8ec160784132126bd2
Ryan Butler <thebutlah@gmail.com>
2023-12-07 23:38:18 -0600

    Added back in kickstart diagnostic keymaps

--- lua/thebutlah/config/keymaps.lua
+++ lua/thebutlah/config/keymaps.lua
@@ -8,7 +8,11 @@
 vim.keymap.set({ 'n', 'v' }, '<C-d>', '<C-d>zz')
 vim.keymap.set({ 'n', 'v' }, '<C-u>', '<C-u>zz')
 
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Not entirely sure what this does, but its from kickstart.nvim
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
