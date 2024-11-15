531047a46645144b986265e6af9058fbd56d099d
Ryan Butler <thebutlah@gmail.com>
2024-11-14 19:54:29 -0500

    Tweak discord rich presence

--- lua/thebutlah/plugins/cord.lua
+++ lua/thebutlah/plugins/cord.lua
@@ -3,6 +3,9 @@ return {
 	build = "./build || .\\build",
 	event = "VeryLazy",
 	opts = {
+		display = {
+			show_time = false,
+		},
 		timer = {
 			reset_on_idle = true, -- Reset start timestamp on idle
 			reset_on_change = false, -- Reset start timestamp on presence change
@@ -10,7 +13,8 @@ return {
 		idle = {
 			enable = true, -- Enable idle status
 			show_status = false, -- Display idle status, disable to hide the rich presence on idle
-			timeout = 100, -- Timeout in milliseconds after which the idle status is set, 0 to display immediately
+			-- Currently there seems to be a bug, 100 = 20 seconds.
+			timeout = 1500, -- Timeout in milliseconds after which the idle status is set, 0 to display immediately
 			disable_on_focus = false, -- Do not display idle status when neovim is focused
 			text = "Idle", -- Text to display when idle
 			tooltip = "💤", -- Text to display when hovering over the idle image
