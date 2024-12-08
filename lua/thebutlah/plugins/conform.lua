7b392cf8d250f65a9ebf5196b68870a775c06d23
Ryan Butler <thebutlah@gmail.com>
2024-12-08 16:03:24 -0600

    added prettier to conform.nvim

--- lua/thebutlah/plugins/conform.lua
+++ lua/thebutlah/plugins/conform.lua
@@ -38,7 +38,8 @@ return {
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			html = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
 }
