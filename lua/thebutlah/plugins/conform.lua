0110a9fa5a79d64cfd0024560570145630f9e99a
Ryan Butler <thebutlah@gmail.com>
2024-12-19 12:18:16 -0500

    added black python autoformatter

--- lua/thebutlah/plugins/conform.lua
+++ lua/thebutlah/plugins/conform.lua
@@ -35,6 +35,7 @@ return {
 			lua = { "stylua" },
 			nix = { "nixpkgs_fmt" },
 			cs = { "csharpier" },
+			python = { "black" },
 
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
