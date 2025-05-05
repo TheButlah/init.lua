0f741db81dd8eb7cde0740a71ffdad04b79384d7
Ryan Butler <thebutlah@gmail.com>
2025-05-04 22:33:56 -0400

    format css with prettier

--- lua/thebutlah/plugins/conform.lua
+++ lua/thebutlah/plugins/conform.lua
@@ -48,8 +48,9 @@ return {
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			javascript = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
 			html = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
 }
